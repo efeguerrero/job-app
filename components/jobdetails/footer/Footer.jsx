b786ae7cc92741161ffff2154d63c1484bf1359c
efeguerrero <juanfg.8640@gmail.com>
2023-10-17 16:43:16 -0300

    job footer done

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native';
 
-import styles from './footer.style'
+import styles from './footer.style';
 
-const Footer = () => {
+//Constants imports
+import { icons } from '../../../constants';
+
+const Footer = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/jobdetails/footer/footer.style.js
+++ components/jobdetails/footer/footer.style.js
@@ -1,40 +1,39 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-import { COLORS, FONT, SIZES } from "../../../constants";
+import { COLORS, FONT, SIZES } from '../../../constants';
 
 const styles = StyleSheet.create({
   container: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
+    // position: 'absolute',
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
     padding: SIZES.small,
-    backgroundColor: "#FFF",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   likeBtn: {
     width: 55,
     height: 55,
     borderWidth: 1,
-    borderColor: "#F37453",
+    borderColor: '#F37453',
     borderRadius: SIZES.medium,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   likeBtnImage: {
-    width: "40%",
-    height: "40%",
-    tintColor: "#F37453",
+    width: '40%',
+    height: '40%',
+    tintColor: '#F37453',
   },
   applyBtn: {
-    flex: 1,
-    backgroundColor: "#FE7654",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginLeft: SIZES.medium,
+    height: 55,
+    width: '80%',
+    backgroundColor: '#FE7654',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: SIZES.medium,
   },
   applyBtnText: {
