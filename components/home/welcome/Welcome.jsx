127c2b7a3465eaedb0bbf2bdf4a26dd9e8d33465
efeguerrero <juanfg.8640@gmail.com>
2023-10-31 11:25:34 -0300

    Welcome Page styling

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -25,7 +25,7 @@ const Welcome = () => {
     <View>
       <View style={styles.container}>
         <Text style={styles.userName}>Welcome to Jobster!</Text>
-        <Text style={styles.welcomeMessage}>Lets find your tech job</Text>
+        <Text style={styles.welcomeMessage}>Time to find your new job.</Text>
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
