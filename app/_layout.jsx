e0a1ea71dded97a4a2ce6355224382317ed71d2d
efeguerrero <juanfg.8640@gmail.com>
2023-11-01 14:00:43 -0300

    new layout config Assets needed for start up fetched in layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,26 +1,60 @@
+import { useCallback, useEffect, useState } from 'react';
 import { Stack } from 'expo-router/stack';
-import { useCallback } from 'react';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
+//Store Import
+import { useSavedJobsStore } from '../store/store';
+
 // Keep the splash screen visible while we fetch resources like fonts, API fetches,etc
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 const AppLayout = () => {
-  const [fontsLoaded] = useFonts({
-    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
+  const getSavedJobs = useSavedJobsStore((state) => state.getSavedJobs);
 
-  //Dont re run between renders. Only when fontsLoaded changes
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        //Pre loading fonts
+        await Font.loadAsync({
+          DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+          DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+          DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+        });
+
+        //Load savedJobs from userStorage
+        await getSavedJobs();
+
+        // Artificially delay for two seconds to avoid snappy loading.
+        //await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.log('there was an error', error);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      setTimeout(await SplashScreen.hideAsync(), 2000);
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      const hideRes = await SplashScreen.hideAsync();
+      console.log('hideRes', hideRes);
     }
-  }, [fontsLoaded]);
+  }, [appIsReady]);
 
-  if (!fontsLoaded) return null;
+  if (!appIsReady) {
+    return null;
+  }
 
   return <Stack onLayout={onLayoutRootView} />;
 };
